96e44a7c8760d6a64dc5d8749832f2165cee3fd8
Kai Kawasaki Ueda <kaiu8x@Kais-MacBook-Pro.local>
2019-10-15 20:03:20 -0500

    create user-settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
+import { UserSettingsComponent } from './components/top-nav-bar/user-settings/user-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventDetailComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    UserSettingsComponent
   ],
   imports: [
     BrowserModule,
