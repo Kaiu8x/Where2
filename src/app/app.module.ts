60d896e5493777491a3f35e0ff682d18273aec9f
Sergio Ugalde <serch037@gmail.com>
2019-10-15 19:51:33 -0500

    Added boilerplate for card detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
