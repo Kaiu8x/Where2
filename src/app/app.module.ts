689d1d5771c69cb4a585ffe757a96f63498f3ff4
Kai Kawasaki Ueda <kaiu8x@Kais-MacBook-Pro.local>
2019-10-15 20:11:53 -0500

    create notifications and general-info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.compone
 import { UserSettingsComponent } from './components/top-nav-bar/user-settings/user-settings.component';
 import { EventsPageComponent } from './components/events-page/events-page.component';
 import { UserPreferencesComponent } from './components/top-nav-bar/user-settings/user-preferences/user-preferences.component';
+import { GeneralInfoComponent } from './components/top-nav-bar/user-settings/general-info/general-info.component';
+import { NotificationsComponent } from './components/top-nav-bar/notifications/notifications.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { UserPreferencesComponent } from './components/top-nav-bar/user-settings
     TopNavBarComponent,
     UserSettingsComponent,
     EventsPageComponent,
-    UserPreferencesComponent
+    UserPreferencesComponent,
+    GeneralInfoComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,
