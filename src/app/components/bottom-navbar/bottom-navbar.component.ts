4fecd81d1cf925c8fdfa230f30700b78f71184f0
Sergio Ugalde <serch037@gmail.com>
2019-12-02 16:06:01 -0600

    WTF

--- src/app/components/bottom-navbar/bottom-navbar.component.ts
+++ src/app/components/bottom-navbar/bottom-navbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-bottom-navbar',
@@ -9,16 +9,15 @@ import { TranslateService } from '@ngx-translate/core';
 export class BottomNavbarComponent implements OnInit {
   public lenguaje = 'en';
 
-  constructor( private translate: TranslateService ) {
+  constructor(private translate: TranslateService) {
     this.translate.setDefaultLang(this.lenguaje);
   }
 
   ngOnInit() {
   }
 
-  public cambiarLenguaje(lang){
+  public cambiarLenguaje(lang) {
     this.lenguaje = lang;
     this.translate.use(lang);
   }
-  
 }
