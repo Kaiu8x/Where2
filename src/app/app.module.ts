738016bc1ede126567e92d2d1355efab8756bcd0
SantiagoGutierrezB <sangb@live.com.mx>
2019-12-03 12:32:29 -0600

    Google API
    Use google maps and google places api, but it doeesn't display on the same map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,47 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {TopNavBarComponent} from './components/top-nav-bar/top-nav-bar.component';
-import {UserSettingsComponent} from './components/top-nav-bar/user-settings/user-settings.component';
-import {EventsPageComponent} from './components/events-page/events-page.component';
-import {EventDetailComponent} from './components/event-detail/event-detail.component';
-import {UserPreferencesComponent} from './components/top-nav-bar/user-settings/user-preferences/user-preferences.component';
-import {GeneralInfoComponent} from './components/top-nav-bar/user-settings/general-info/general-info.component';
-import {NotificationsComponent} from './components/top-nav-bar/notifications/notifications.component';
-import {BottomNavbarComponent} from './components/bottom-navbar/bottom-navbar.component';
-import {HomeComponent} from './components/home/home.component';
-import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
-import {CardComponent} from './components/home/card/card.component';
-import {CalendarComponent} from './components/home/calendar/calendar.component';
-import {SignInComponent} from './components/sign-in/sign-in.component';
-import {RegisterComponent} from './components/register/register.component';
-import {GraphsComponent} from './components/graphs/graphs.component';
-import {IonicModule} from '@ionic/angular';
-import {AgmCoreModule} from '@agm/core';
-import {DiscoverComponent} from './components/discover/discover.component';
-import {ProfileComponent} from './components/profile/profile.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TopNavBarComponent } from "./components/top-nav-bar/top-nav-bar.component";
+import { UserSettingsComponent } from "./components/top-nav-bar/user-settings/user-settings.component";
+import { EventsPageComponent } from "./components/events-page/events-page.component";
+import { EventDetailComponent } from "./components/event-detail/event-detail.component";
+import { UserPreferencesComponent } from "./components/top-nav-bar/user-settings/user-preferences/user-preferences.component";
+import { GeneralInfoComponent } from "./components/top-nav-bar/user-settings/general-info/general-info.component";
+import { NotificationsComponent } from "./components/top-nav-bar/notifications/notifications.component";
+import { BottomNavbarComponent } from "./components/bottom-navbar/bottom-navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { CardComponent } from "./components/home/card/card.component";
+import { CalendarComponent } from "./components/home/calendar/calendar.component";
+import { SignInComponent } from "./components/sign-in/sign-in.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { GraphsComponent } from "./components/graphs/graphs.component";
+import { IonicModule } from "@ionic/angular";
+import { AgmCoreModule } from "@agm/core";
+import { DiscoverComponent } from "./components/discover/discover.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { GooglePlaceModule } from "node_modules/ngx-google-places-autocomplete";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 const appRoutes: Routes = [
-  {path: 'users/:id/settings', component: UserSettingsComponent},
-  {path: 'users/:id', component: GeneralInfoComponent},
-  {path: 'events/:id', component: EventDetailComponent},
-  {path: 'events', component: EventsPageComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'discover', component: DiscoverComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'login', component: PageNotFoundComponent},
-  {path: 'register', component: PageNotFoundComponent},
-  {path: '**', component: PageNotFoundComponent},
-]
+  { path: "users/:id/settings", component: UserSettingsComponent },
+  { path: "users/:id", component: GeneralInfoComponent },
+  { path: "events/:id", component: EventDetailComponent },
+  { path: "events", component: EventsPageComponent },
+  { path: "home", component: HomeComponent },
+  { path: "discover", component: DiscoverComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "login", component: PageNotFoundComponent },
+  { path: "register", component: PageNotFoundComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -60,18 +62,16 @@ const appRoutes: Routes = [
     RegisterComponent,
     GraphsComponent,
     DiscoverComponent,
-    ProfileComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes,
-      {enableTracing: true}
-    ),
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
     HttpClientModule,
     TranslateModule.forRoot({
-      loader:{
+      loader: {
         provide: TranslateLoader,
         useFactory: (http: HttpClient) => {
           return new TranslateHttpLoader(http);
@@ -81,9 +81,12 @@ const appRoutes: Routes = [
     }),
     IonicModule.forRoot(),
     AgmCoreModule.forRoot({
-        apiKey: 'AIzaSyDh_7kD-kYAlIYjWRXbHZvO6t2UjtFrmNQ',
-      }
-    )
+      apiKey: "AIzaSyDh_7kD-kYAlIYjWRXbHZvO6t2UjtFrmNQ",
+      libraries: ["places"]
+    }),
+    FormsModule,
+    ReactiveFormsModule,
+    GooglePlaceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/discover/discover.component.ts
+++ src/app/components/discover/discover.component.ts
@@ -1,15 +1,292 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+// import {} from "googlemaps";
+
+// declare var google;
+// let map: any;
+// let infowindow: any;
+// let options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0
+// };
+// @Component({
+//   selector: "app-discover",
+//   templateUrl: "./discover.component.html",
+//   styleUrls: ["./discover.component.scss"]
+// })
+// export class DiscoverComponent implements OnInit {
+//   @ViewChild("map", { static: false }) mapElement: ElementRef;
+//   map: google.maps.Map;
+//   formattedAddress = "";
+
+//   options = {
+//     componentRestrictions: {
+//       country: ["MX"]
+//     }
+//   };
+
+//   constructor() {}
+
+//   ngOnInit() {}
+
+//   ngAfterViewInit() {
+//     setTimeout(() => {
+//       this.initMap2();
+//     }, 1000);
+//   }
+
+//   public initMap() {
+//     const mapProperties = {
+//       center: new google.maps.LatLng(19.2864807, -99.1384592),
+//       zoom: 15,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//     this.map = new google.maps.Map(
+//       this.mapElement.nativeElement,
+//       mapProperties
+//     );
+//   }
+
+//   initMap2() {
+//     navigator.geolocation.getCurrentPosition(
+//       location => {
+//         console.log(location);
+//         map = new google.maps.Map(this.mapElement.nativeElement, {
+//           center: {
+//             lat: location.coords.latitude,
+//             lng: location.coords.longitude
+//           },
+//           zoom: 15
+//         });
+
+//         infowindow = new google.maps.InfoWindow();
+//         var service = new google.maps.places.PlacesService(map);
+//         service.nearbySearch(
+//           {
+//             location: {
+//               lat: location.coords.latitude,
+//               lng: location.coords.longitude
+//             },
+//             radius: 1000,
+//             type: ["store"]
+//           },
+//           (results, status) => {
+//             if (status === google.maps.places.PlacesServiceStatus.OK) {
+//               for (var i = 0; i < results.length; i++) {
+//                 this.createMarker(results[i]);
+//               }
+//             }
+//           }
+//         );
+//       },
+//       error => {
+//         console.log(error);
+//       },
+//       options
+//     );
+//     var myplace = { lat: -33.8665, lng: 151.1956 };
+//   }
+//   createMarker(place) {
+//     var placeLoc = place.geometry.location;
+//     var marker = new google.maps.Marker({
+//       map: map,
+//       position: placeLoc
+//     });
+
+//     google.maps.event.addListener(marker, "click", function() {
+//       infowindow.setContent(place.name);
+//       infowindow.open(map, this);
+//     });
+//   }
+
+//   public handleAddressChange(address: any) {
+//     this.formattedAddress = address.formatted_address;
+//   }
+// }
+
+import {
+  Component,
+  ElementRef,
+  NgZone,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { MapsAPILoader } from "@agm/core";
+import { } from "googlemaps";
+
+declare var google;
+let map: any;
+let infowindow: any;
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
 
 @Component({
-  selector: 'app-discover',
-  templateUrl: './discover.component.html',
-  styleUrls: ['./discover.component.scss']
+  selector: "app-discover",
+  templateUrl: "./discover.component.html",
+  styleUrls: ["./discover.component.scss"]
 })
 export class DiscoverComponent implements OnInit {
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+  previous;
 
-  constructor() { }
+  @ViewChild("map", { static: false }) mapElement: ElementRef;
+  map: google.maps.Map;
 
-  ngOnInit() {
+  public options = {
+    types: ["establishment"],
+    componentRestrictions: {
+      country: ["MX"]
+    }
+  };
+
+  clickedMarker(infowindow) {
+    if (this.previous) {
+      this.previous.close();
+    }
+    this.previous = infowindow;
   }
 
+  markerOver(m: marker) {
+    m.animation = 'BOUNCE';
+  }
+
+  markerOut(m: marker) {
+    m.animation = '';
+  }
+
+  markers: marker[] = [
+    // {
+    //   lat: 51.673858,
+    //   lng: 7.815982,
+    //   label: 'A',
+    //   animation: 'DROP'
+    // },
+    // {
+    //   lat: 51.373858,
+    //   lng: 7.215982,
+    //   label: 'B',
+    //   animation: 'DROP'
+    // },
+    // {
+    //   lat: 51.723858,
+    //   lng: 7.895982,
+    //   label: 'C',
+    //   animation: 'DROP'
+    // }
+  ]
+
+  @ViewChild("search", { static: false })
+  public searchElementRef: ElementRef;
+
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) { }
+
+  ngOnInit() {
+    //set google maps defaults
+    this.zoom = 4;
+    this.latitude = 19.2864807;
+    this.longitude = -99.1384592;
+
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //set current position
+    this.setCurrentPosition();
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(
+        this.searchElementRef.nativeElement,
+        this.options
+      );
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.initMap();
+    }, 1000);
+  }
+
+  public initMap() {
+
+
+    map = new google.maps.Map(this.mapElement.nativeElement, {
+      center: {
+        lat: this.latitude,
+        lng: this.longitude
+      },
+      zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(
+      {
+        location: {
+          lat: this.latitude,
+          lng: this.longitude
+        },
+        radius: 1000,
+      },
+      (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+        }
+      }
+    );
+  }
+
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
+  }
+
+  createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeLoc
+    });
+
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+}
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  animation: 'DROP' | 'BOUNCE' | '';
 }
