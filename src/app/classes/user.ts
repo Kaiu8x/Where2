a179621c9ba4608c98aa55786f6e3b69e7fc81ba
Sergio Ugalde <serch037@gmail.com>
2019-12-08 17:36:30 -0600

    Changed user fields

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,9 +1,9 @@
-import {Event} from './event';
+import { Event } from './event';
 export class User {
   id: string;
   firstName: string;
   lastName: string;
-  events: Event[];
-  friends: User[];
-  profileImg: string;
+  email: string;
+  friends: string[];
+  photoUrl: string;
 }
